1c997375e4ae59170d4425a373f62e20764146d7
Buvanesh pulugarajan <buvaneshpugazhenthi@gmail.com>
2026-02-02 17:15:44 +0530

    model committed

---
+++ Model.py
@@ -0,0 +1,15 @@
+from pydantic import BaseModel
+
+
+
+class model(BaseModel):
+
+    property_id:str
+    city:str
+    commercial_type:str
+    size_sqm:int
+    annual_rent:int
+    occupancy_status:str
+    lease_term_years:int
+
+
--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import uvicorn 
 from fastapi import FastAPI
 import json 
+from Model import model
+import numpy as np 
 
 
 app=FastAPI()
 
 file=open('properties.json','r')
 properties=json.load(file)
+
+
+
 
 
 
@@ -31,6 +36,37 @@ def get_top():
 @app.get("/properties/all")
 def others():
     return properties
+
+
+@app.post('/predict')
+def predict(data:model):
+      data=data.model_dump()
+      property_id=data.property_id
+      city=data.city
+      commercial_type=data.commercial_type
+      size_sqm=data.size_sqm
+      annual_rent=data.annual_rent
+      occupancy_status=data.occupancy_status
+      lease_term_years=data.lease_term_years
+
+      occupancy_status=(1 if occupancy_status=='vacant' else 0)  # array(['office', 'retail_shop', 'showroom', 'warehouse'], dtype=object)
+      com_arr=['office', 'retail_shop', 'showroom', 'warehouse']
+      
+      com_map = {value: idx for idx, value in enumerate(com_arr)}
+
+      rent_sqm=annual_rent/size_sqm
+      lease_term_short=np.where 
+
+
+
+       
+      
+
+            
+          
+    
+
+
 
 
 if __name__=="__main__":
